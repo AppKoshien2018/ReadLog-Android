e1b4fa2e06b85eaebe2c008949adbdb95bb0cd03
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-08-10 18:06:10 +0900

    I implemented a page transition to the login screen.

--- app/src/main/java/com/yoshi1125hisa/readlog/OpActivity.java
+++ app/src/main/java/com/yoshi1125hisa/readlog/OpActivity.java
@@ -1,19 +1,34 @@
 package com.yoshi1125hisa.readlog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.RelativeLayout;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class OpActivity extends AppCompatActivity {
-
+    RelativeLayout relativeLayout;
 //    private boolean clickedFav = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+            //ページ遷移のアニメーション。
         setContentView(R.layout.activity_op);
+
+        relativeLayout = findViewById(R.id.relativeLayout);
+
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OpActivity.this,SignActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 //        final LottieAnimationView animationView = findViewById(R.id.animation_view);
--- app/src/main/java/com/yoshi1125hisa/readlog/SignActivity.java
+++ app/src/main/java/com/yoshi1125hisa/readlog/SignActivity.java
@@ -1,13 +1,28 @@
 package com.yoshi1125hisa.readlog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class SignActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        //ページ遷移のアニメーション。
         setContentView(R.layout.activity_sign);
+    }
+
+
+    public void signin(View v){
+
+    }
+
+
+    public void signup(View v){
+
     }
 }
