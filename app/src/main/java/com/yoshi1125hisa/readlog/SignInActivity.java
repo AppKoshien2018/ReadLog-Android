90642933df90698ef9971aa401dcf78ecc0f23d0
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-08-11 08:39:22 +0900

    Add Complete activity and Main activity views.

--- app/src/main/java/com/yoshi1125hisa/readlog/SignInActivity.java
+++ app/src/main/java/com/yoshi1125hisa/readlog/SignInActivity.java
@@ -1,7 +1,9 @@
 package com.yoshi1125hisa.readlog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class SignInActivity extends AppCompatActivity {
@@ -12,5 +14,10 @@ public class SignInActivity extends AppCompatActivity {
         getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
         //ページ遷移のアニメーション。
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void signin(View v){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/yoshi1125hisa/readlog/SignUpActivity.java
+++ app/src/main/java/com/yoshi1125hisa/readlog/SignUpActivity.java
@@ -1,7 +1,9 @@
 package com.yoshi1125hisa.readlog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -12,5 +14,10 @@ public class SignUpActivity extends AppCompatActivity {
         getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
         //ページ遷移のアニメーション。
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void signup(View v){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
