a4ed58f1afb06404dd6f08985077fbe16e42d317
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-08-11 14:43:44 +0900

    Add done animation for complete view.

--- app/src/main/java/com/yoshi1125hisa/readlog/SignUpActivity.java
+++ app/src/main/java/com/yoshi1125hisa/readlog/SignUpActivity.java
@@ -17,7 +17,7 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     public void signup(View v){
-        Intent intent = new Intent(this,MainActivity.class);
+        Intent intent = new Intent(this,CompletionActivity.class);
         startActivity(intent);
     }
 }
