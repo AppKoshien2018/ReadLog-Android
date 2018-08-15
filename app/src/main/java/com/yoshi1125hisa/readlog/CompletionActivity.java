3c07e6523d2eb363a21c070c79efd2337275effb
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-08-15 22:46:15 +0900

    Add splash view.

--- app/src/main/java/com/yoshi1125hisa/readlog/CompletionActivity.java
+++ app/src/main/java/com/yoshi1125hisa/readlog/CompletionActivity.java
@@ -2,40 +2,43 @@ package com.yoshi1125hisa.readlog;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class CompletionActivity extends AppCompatActivity {
+public class CompletionActivity extends AppCompatActivity{
+
+    TextView doneText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        //ページ遷移のアニメーション。
         setContentView(R.layout.activity_completion);
 
-        TextView doneText;
+
         doneText = findViewById(R.id.doneText);
         doneText.setTypeface(Typeface.createFromAsset(getAssets(),
                 "hiragino.ttf"));
 
-        RelativeLayout relativeLayout = findViewById(R.id.relativeLayout);
-        relativeLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // もしログインしているなら、MainActivityに移動する。
-
-                // そうでないなら、SignActivityに移動する。
-                Intent intent = new Intent(CompletionActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        // もしアニメーションが終了して入れば、次のActivityへ移動する。
+        Handler hdl = new Handler();
+        // 第２引数で切り替わる秒数(ミリ秒)を指定、今回は5秒
+        hdl.postDelayed(new splashHandler(), 5000);
+    }
+    // splashHandlerクラス
+    class splashHandler implements Runnable {
+        public void run() {
+            Intent intent = new Intent(CompletionActivity.this, MainActivity.class);
+            startActivity(intent);
+            CompletionActivity.this.finish();
+        }
     }
 }
